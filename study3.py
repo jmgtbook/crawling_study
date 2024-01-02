492b6fdb4d1997925610714f607ae23a94956f97
남민우 <jmgtbook@gmail.com>
2024-01-02 14:32:02 +0000

    FIX: 최신글 확인 알고리즘 최적화

--- study3.py
+++ study3.py
@@ -4,8 +4,8 @@ import time
 
 target_link = 'https://news.daum.net/breakingnews/economic'
 
-def appendTitleToList(news_title_list: list, news_title_string: str, news) -> None:
-    news_title_link = news.get('href')
+def appendTitleToList(news_title_list: list, news: str, news_title_link) -> None:
+    news_title_string = news.get_text()
     news_title_list.append({'제목':news_title_string,'링크':news_title_link})
     print(news_title_list[-1])
     
@@ -21,8 +21,7 @@ news_title_list = []
 for i, news_title in enumerate(news):
     news_title_string = news_title.get_text()
     news_title_link=news_title.get('href')
-    news_title_list.insert(-1,{'제목':news_title_string,'링크':news_title_link})
-    
+    news_title_list.insert(0,{'제목':news_title_string,'링크':news_title_link})
     
 for i in news_title_list:
     print(i)
@@ -36,28 +35,19 @@ while True:
     main_news = soup.find(class_='list_news2 list_allnews')
 
     news = main_news.find_all('a',class_='link_txt', limit=3)
-    news_title_string=[]
-    news_list=[]
+    news_title_string = []
+    news_link_list = []
     for news_one in news:
-        news_title_string.append(news_one.get_text())
-        news_list.append(news_one)
-        
-    if news_title_list[-1]['제목'] == news_title_string[0]:
-        continue
-    elif news_title_list[-1]['제목'] == news_title_string[1]:
-        appendTitleToList(news_title_list,news_title_string[0],news_list[0])
-        
-    elif news_title_list[-1]['제목'] == news_title_string[2]:
-        appendTitleToList(news_title_list,news_title_string[1],news_list[1])
-        print('\n')
-        appendTitleToList(news_title_list,news_title_string[0],news_list[0])
-        
-    else:
-        appendTitleToList(news_title_list,news_title_string[2],news_list[2])
-        print('\n')
-        appendTitleToList(news_title_list,news_title_string[1],news_list[1])
-        print('\n')
-        appendTitleToList(news_title_list,news_title_string[0],news_list[0])
-     
-
+        news_title_string.append(news_one)
+        news_link_list.append(news_one.get('href'))
     
+    cnt = 0
+    for i,news_link_list_one in enumerate(news_link_list):
+        if news_title_list[-1]['링크'] == news_link_list_one:
+            break
+        else:
+            cnt+=1
+    for i in range(cnt):
+        appendTitleToList(news_title_list,news_title_string[cnt-i-1],news_link_list[cnt-i-1])
+    
+            
